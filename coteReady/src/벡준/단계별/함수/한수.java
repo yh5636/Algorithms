00f978941db8482e28a377a87593e38272e4dac2
yh5636 <yh5636@naver.com>
2022-08-23 19:49:53 +0900

    자리수 구하기, 함수

--- coteReady/src/벡준/단계별/함수/한수.java
+++ coteReady/src/벡준/단계별/함수/한수.java
@@ -7,25 +7,31 @@ public class 한수 {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = br.read();
+		int n = Integer.parseInt(br.readLine());
 		br.close();
 
 		int rst = 0;
+		hanSuF(n);
+	}
 
+	static void hanSuF(int n) {
 		// 1~99까지는 무조건 등차수열(두개의 수만 주어지니까)
 		// 100이상의 수부터는 한자리씩 비교
 		if (n < 100) {
 			System.out.print(n);
 		} else {
-			int n1 = n / 100; // 첫번째 자리
-			int n2 = (n % 100) / 10; // 두번째 자리
-			int n3 = (n % 100) % 10; // 세번째 자리
+			int cnt = 99;
+			for (int i = 100; i <= n; i++) {
+				int n1 = i / 100; // 첫번째 자리
+				int n2 = i / 10 % 10; // 두번째 자리
+				int n3 = i % 10; // 세번째 자리
 
-			if (n1 - n2 == n2 - n3) {
-				rst++;
+				if ((n1 - n2) == (n2 - n3)) {
+					cnt++;
+				}
 			}
+			System.out.print(cnt);
 		}
 
-		System.out.print(rst);
 	}
 }
